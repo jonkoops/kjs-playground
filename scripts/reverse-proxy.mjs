1f3f0d1d1f4a71e5fd5037e493b3b7be1f57c16c
Jon Koops <jonkoops@gmail.com>
2023-03-28 13:26:33 +0200

    Add script to proxy Vite and Keycloak

---
+++ scripts/reverse-proxy.mjs
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+// @ts-check
+import { createServer } from 'node:https';
+import httpProxy from 'http-proxy';
+import mkcert from 'mkcert';
+
+const PORT = 8080
+const PROXIES = new Map([
+  ['keycloak-server.local:8080', 'http://127.0.0.1:8180'],
+  ['kjs-playground.local:8080', 'http://localhost:5173'],
+])
+
+const authority = await mkcert.createCA({
+  organization: 'Fake Certificate Inc.',
+  countryCode: 'NL',
+  state: 'Noord-Holland',
+  locality: 'Amsterdam',
+  validityDays: 365
+});
+
+const certificate = await mkcert.createCert({
+  domains: ['127.0.0.1', 'localhost', '*.local'],
+  validityDays: 365,
+  caKey: authority.key,
+  caCert: authority.cert
+});
+
+const proxyServer = httpProxy.createProxyServer({
+  xfwd: true
+})
+
+const server = createServer(certificate, (req, res) => {
+  const host = req.headers.host
+  const target = host ? PROXIES.get(host) : undefined;
+
+  if (!target) {
+    throw new Error(`Unable to proxy to ${host}, no target found.`)
+  }
+
+  proxyServer.web(req, res, { target })
+})
+
+server.listen(PORT)
+console.log(`listening on port ${PORT}`)
