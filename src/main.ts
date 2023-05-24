25692dc1b78e2452a3f7c6e2aebdd21590db1127
Jon Koops <jonkoops@gmail.com>
2023-05-24 17:30:50 +0200

    Replace Keycloak JS with `oidc-client-ts`

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,7 @@
-import Keycloak from "keycloak-js"
+import {UserManager} from "oidc-client-ts"
 
-const keycloak = new Keycloak({
-  url: 'https://keycloak-server.local:8080',
-  realm: 'master',
-  clientId: 'kjs-playground'
-})
-
-const loginButton = document.getElementById('login')!
-const logoutButton = document.getElementById('logout')!
-const updateTokenButton = document.getElementById('updateToken')!
-const infoText = document.getElementById('info')!
-
-loginButton.addEventListener('click', () => keycloak.login())
-logoutButton.addEventListener('click', () => keycloak.logout({ redirectUri: window.location.origin }))
-updateTokenButton.addEventListener('click', () => keycloak.updateToken(9999))
-
-function updateScreen() {
-  if (keycloak.authenticated) {
-    infoText.textContent = "User is authenticated."
-  } else {
-    infoText.textContent = "User is not authenticated."
-  }
-}
-
-const handleEvent = (eventName: string) => (...params: unknown[]) => {
-  console.log(eventName, ...params)
-  updateScreen()
-}
-
-keycloak.onReady = handleEvent('Ready')
-keycloak.onAuthSuccess = handleEvent('AuthSuccess')
-keycloak.onAuthError = handleEvent('AuthError')
-keycloak.onAuthRefreshSuccess = handleEvent('AuthRefreshSuccess')
-keycloak.onAuthRefreshError = handleEvent('AuthRefreshError')
-keycloak.onAuthLogout = handleEvent('AuthLogout')
-keycloak.onTokenExpired = handleEvent('TokenExpired')
-
-try {
-  await keycloak.init({
-    onLoad: 'check-sso',
-    silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
-    enableLogging: true
-  })
-} catch (error) {
-  console.error('Unable to initialize Keycloak:', error)
-}
+const userManager = new UserManager({
+  authority: 'https://keycloak-server.local:8080/realms/master',
+  client_id: 'kjs-playground',
+  redirect_uri: location.href
+});
